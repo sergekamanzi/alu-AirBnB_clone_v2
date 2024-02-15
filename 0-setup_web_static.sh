49aeb7d196b0e4333a3e5451c4faf9567b27c333
sergekamanzi <s.kamanzi@alustudent.com>
2024-02-15 19:34:20 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,38 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# sets up the web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Webstatic_deployment" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
-=======
-# Install nginx if not already installed
+apt-get update
+apt-get install -y nginx
 
-if ! [ -x "$(command -v nginx)" ]; then
-	  sudo apt-get update
-	    sudo apt-get -y install nginx
-fi
-sudo service nginx start
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo echo "<html>
-  <head>
-    </head>
-      <body>
-          Holberton School
-	    </body>
-	    </html>" | sudo tee /data/web_static/releases/test/index.html
-	    sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-	    sudo chown -R ubuntu:ubuntu /data/
-	    sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-	    sudo service nginx restart
->>>>>>> a717ed48f2df90b26c7fcaf70bc9ab94c06a2385
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
