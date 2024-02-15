151d0c05ea5effec7123fb8dda25702d4ceec3ee
sergekamanzi <s.kamanzi@alustudent.com>
2024-02-15 17:03:15 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder"""
+
 from fabric.api import local
+from datetime import datetime
+import os
 
 
 def do_pack():
-        """Create a tar gzipped archive of the directory web_static."""
-            dt = datetime.utcnow()
-                file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                                                 dt.month,
-                                                                                                                                          dt.day,
-                                                                                                                                                                                                   dt.hour,
-                                                                                                                                                                                                                                                            dt.minute,
-                                                                                                                                                                                                                                                                                                                     dt.second)
-                    if os.path.isdir("versions") is False:
-                                if local("mkdir -p versions").failed is True:
-                                                return None
-                                                if local("tar -cvzf {} web_static".format(file)).failed is True:
-                                                            return None
-                                                            return file e
+        """Function to generate a .tgz archive from the contents of the web_static
+            folder."""
+
+                time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                    local("mkdir -p versions")
+                        archive_path = "versions/web_static_{}.tgz".format(time_stamp)
+                            local("tar -cvzf {} web_static".format(archive_path))
+                                if os.path.exists(archive_path):
+                                            return archive_path
+                                            else:
+                                                        return None
