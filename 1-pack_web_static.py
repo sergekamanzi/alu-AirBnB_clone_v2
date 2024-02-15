a717ed48f2df90b26c7fcaf70bc9ab94c06a2385
sergekamanzi <s.kamanzi@alustudent.com>
2024-02-15 17:16:47 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,11 +11,11 @@ def do_pack():
         """Function to generate a .tgz archive from the contents of the web_static
             folder."""
 
-                time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-                    local("mkdir -p versions")
-                        archive_path = "versions/web_static_{}.tgz".format(time_stamp)
-                            local("tar -cvzf {} web_static".format(archive_path))
-                                if os.path.exists(archive_path):
-                                            return archive_path
-                                            else:
-                                                        return None
+    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    archive_path = "versions/web_static_{}.tgz".format(time_stamp)
+    local("tar -cvzf {} web_static".format(archive_path))
+    if os.path.exists(archive_path):
+       return archive_path
+    else:
+       return None
