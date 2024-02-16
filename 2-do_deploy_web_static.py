4dc517cfbc791daf72d4426309d18ca8b31e794c
sergekamanzi <s.kamanzi@alustudent.com>
2024-02-16 18:12:02 +0000

    2 do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,54 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+Write a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy:
+"""
+from fabric.api import *
+import os
 
-env.hosts = ["54.146.252.71", "50.19.162.195"]
+
+env.hosts = ["ubuntu@54.227.177.156", "ubuntu@54.88.196.86"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    Prototype: def do_deploy(archive_path):
+    Returns False if the file at the path archive_path doesn't exist
+    The script should take the following steps:
+    Upload the archive to the /tmp/ directory of the web server
+    Uncompress the archive to the folder
+    /data/web_static/releases/<archive filename without extension>
+    on the web server Delete the archive from the web server
+    Delete the symbolic link /data/web_static/current from the web server
+    Create a new the symbolic link /data/web_static/current on the web server,
+    linked to the new version of your code
+    (/data/web_static/releases/<archive filename without extension>)
+    All remote commands must be executed on your both web servers
+    (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+    Returns True if all operations have been done correctly,
+    otherwise returns False You must use this script to deploy
+    it on your servers: xx-web-01 and xx-web-02
+    """
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    archive = archive_path.split('/')[-1]
+    filename_folder = archive.split('.')[0]
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{0}".format(filename_folder))
+        run("tar -C /data/web_static/releases/{0} \
+            -xzvf /tmp/{1}".format(filename_folder, archive))
+        run("rm /tmp/{0}".format(archive))
+        run("mv /data/web_static/releases/{0}/web_static/* \
+            /data/web_static/releases/{1}/".format(filename_folder,
+                                                   filename_folder))
+        run("rm -rf \
+            /data/web_static/releases/{0}/web_static".format(filename_folder))
+        run("rm /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{0} \
+            /data/web_static/current".format(filename_folder))
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+    else:
+        return True
